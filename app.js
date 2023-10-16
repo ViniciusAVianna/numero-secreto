e6824b7bfde319c6f670cabf03feba5d7ebef0cf
ViniciusAVianna <vinicius_alves_vianna@hotmail.com>
2023-10-15 22:50:27 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,69 @@
+let numerosJaSorteados = [];
+let tentativas = 0;
+let numeroMaximo = 10;
+
+function exibirTexto(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, "Brazilian Portuguese Female", {rate:1.2});
+}
+
+function exibirTextoInicial(){
+    exibirTexto("h1", "Jogo do número secreto");
+    exibirTexto("p", "Escolha um número entre 1 e " + numeroMaximo);  
+}
+exibirTextoInicial();
+
+
+function gerarNumeroAleatorio(){
+    let numeroGerado = parseInt(Math.random() * numeroMaximo + 1);
+
+    if (numerosJaSorteados.length == numeroMaximo){
+        numerosJaSorteados = [];
+    }
+
+    if (numerosJaSorteados.includes(numeroGerado)){
+        return gerarNumeroAleatorio();
+    } else{
+        numerosJaSorteados.push(numeroGerado);
+        console.log(numerosJaSorteados);
+        return numeroGerado;
+    }   
+}
+let numSecreto = gerarNumeroAleatorio();
+
+
+function limparCampo(){
+    palpite = document.querySelector("input");
+    palpite.value = "";
+}
+
+
+function verificarChute(){
+    tentativas++;
+    let palpite = document.querySelector("input").value;
+    console.log(numSecreto);
+    
+    if (palpite == numSecreto){
+        exibirTexto("h1", "Acertou!");
+        let palavraTentativa = tentativas == 1 ? "tentativa" : "tentativas";
+        let msgAcerto = `Você descobriu o número secreto em ${tentativas} ${palavraTentativa}.`
+        exibirTexto("p", msgAcerto);
+        document.getElementById("reiniciar").removeAttribute("disabled");
+    } else if (palpite > numSecreto){
+        exibirTexto("p", "O número secreto é menor que " + palpite);
+        limparCampo();
+    } else {
+        exibirTexto("p", "O número secreto é maior que " + palpite);
+        limparCampo();
+    }
+}
+
+
+function reiniciarJogo(){
+    exibirTextoInicial();
+    numSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 0;
+    document.getElementById("reiniciar").setAttribute("disabled",true);
+}
